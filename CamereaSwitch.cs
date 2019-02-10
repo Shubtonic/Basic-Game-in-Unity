c3e20b22ce2b8ee1074b5d0764e499fbe83af58f
Shubham Pareek <46500753+Shubtonic@users.noreply.github.com>
2019-02-10 13:45:14 +0000

    Add files via upload

---
+++ CamereaSwitch.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamereaSwitch : MonoBehaviour {
+    [SerializeField]
+    Camera FirstPCamera;
+    [SerializeField]
+    Camera ThirdPCamera;
+    [SerializeField]
+    Camera BackCamera;
+    private bool switchCam = false;
+    private bool backCam = false;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        FirstPCamera.GetComponent<Camera>().enabled = false;
+        ThirdPCamera.GetComponent<Camera>().enabled = true;
+        BackCamera.GetComponent<Camera>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    if (Input.GetKeyDown("t"))
+        {
+            switchCam = !switchCam;
+            backCam = false;
+        }
+
+        if (Input.GetKeyDown("b"))
+        {
+            switchCam = false;
+            backCam = true;
+        }
+    if(switchCam == true) 
+        {
+            FirstPCamera.GetComponent<Camera>().enabled = true;
+            ThirdPCamera.GetComponent<Camera>().enabled = false;
+            BackCamera.GetComponent<Camera>().enabled = false;
+        }
+        else if(backCam == true)
+        {
+            FirstPCamera.GetComponent<Camera>().enabled = false;
+            ThirdPCamera.GetComponent<Camera>().enabled = false;
+            BackCamera.GetComponent<Camera>().enabled = true;
+        }
+        else
+        {
+            FirstPCamera.GetComponent<Camera>().enabled = false;
+            ThirdPCamera.GetComponent<Camera>().enabled = true;
+            BackCamera.GetComponent<Camera>().enabled = false;
+        }
+	}
+}
---
+++ MeleeSystem.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var TheDamage : int = 25; 
+var Distance : float;
+var MaxDistance : float = 1.5;
+var TheMace : Transform;
+
+function Update() {
+  if (Input.GetButtonDown("Fire1"))
+  {  
+     TheMace.GetComponent.<Animation>().Play("AttackAnimation001");
+     var hit : RaycastHit;                 
+     if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), hit))
+	 {
+	  Distance = hit.distance;
+	  if (Distance < MaxDistance)
+	  {
+
+	   hit.transform.SendMessage("ApplyDamage", TheDamage, SendMessageOptions.DontRequireReceiver);
+	  }
+	 }
+  } 
+   if (TheMace.GetComponent.<Animation>().isPlaying == false)
+   { 
+    TheMace.GetComponent.<Animation>().CrossFade("idle"); 
+   }
+	
+   if (Input.GetKey (KeyCode.LeftShift)) 
+   { 
+    TheMace.GetComponent.<Animation>().CrossFade("HumanoidRun"); 
+   } 
+   
+   if (Input.GetKeyUp(KeyCode.LeftShift)) 
+   { 
+    TheMace.GetComponent.<Animation>().CrossFade("HumanoidRun"); 
+   }
+
+  }
